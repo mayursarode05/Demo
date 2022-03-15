1634dcc33b68ef6145efafb7dd0bf8f7d79531cc
Mayur Sarode <101618743+mayursarode05@users.noreply.github.com>
2022-03-15 12:10:22 +0530

    Add files via upload

---
+++ Mul.java
@@ -0,0 +1,12 @@
+class Mul
+{
+  static void fun(int A,int B,int c)
+   {
+     int result=A*B*c;
+     System.out.println("Multyply="+result);
+   }
+ public static void main(String args[])
+        {
+          fun(3,2,5);
+        }
+}
