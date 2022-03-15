d39506f4e70dd987d59c11464203b1497a23c54b
Mayur Sarode <101618743+mayursarode05@users.noreply.github.com>
2022-03-15 13:32:27 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,7 @@
+class A
+{
+    public static void main(String agrs[])
+{
+ System.out.println("Hello All of us");
+}
+}
---
+++ Sim.java
@@ -0,0 +1,9 @@
+class Sim
+{
+public static void main(String args[])
+{
+System.out.println("Mayur B Sarode");
+System.out.println("Civil Engg");
+System.out.println("SSGB collage of Engg");
+}
+}
