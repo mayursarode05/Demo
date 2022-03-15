2e92b59f24ab912fcb3b6fba873ed00600d7b229
Mayur Sarode <101618743+mayursarode05@users.noreply.github.com>
2022-03-15 13:36:56 +0530

    Add files via upload

---
+++ star.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int i,j,num
+printf("/nEnter your number");
+scanf("Number=,&num");
+   {
+for(i=0,i>num,i++)
+   }
+   {
+   for(j=0,j>num,j++)
+   }
+   {
+     printf("*");
+   }
+}
